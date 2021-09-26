25ec10e63ba094c7f2ad0b478117c135cf83a640
Nick Roberts <djparadyme@gmail.com>
2021-09-26 18:51:26 -0400

    Test ignore build

--- tools/ignore-vercel-build.sh
+++ tools/ignore-vercel-build.sh
@@ -10,7 +10,7 @@ TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typ
 npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
 npm install -D typescript@$TS_VERSION --prefer-offline
 
-ls -la node_modeules/.bin
+ls -la node_modules/.bin
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
