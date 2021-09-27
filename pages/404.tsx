c57ce811373e97b3c1d46cdfcbc05a610e5fe591
Nick Roberts <djparadyme@gmail.com>
2021-09-26 20:52:44 -0400

    Update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,17 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+// import { motion } from "framer-motion";
+// import { forwardRef } from "react";
+
+// const LogoImage = forwardRef<HTMLDivElement>((props, ref) => (
+//   <div ref={ref} {...props}>
+//     <Image height="60" width="60" src="/logo-mark.svg" alt="Nicholas Jay" />
+//   </div>
+// ));
+// LogoImage.displayName = "LogoImage";
+
+// const MotionLogo = motion(LogoImage);
 
 export default function Home() {
   return (
@@ -15,10 +26,20 @@ export default function Home() {
             <div className="flex-shrink-0 flex justify-center">
               <span className="sr-only">Nicholas Jay</span>
               <Image
-                className="h-12 w-auto"
+                height="60"
+                width="60"
                 src="/logo-mark.svg"
                 alt="Nicholas Jay"
               />
+              {/* <MotionLogo
+                variants={{
+                  hidden: { opacity: 0, y: -50 },
+                  show: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                animate="show"
+                transition={{ ease: "easeInOut", duration: 0.125 }}
+              /> */}
             </div>
             <p className="mt-3 text-sm font-semibold text-gray-50 uppercase tracking-wide">
               404 error
